df4bf0f4549b443afe9939718397a7744802bf77
Babenko Liudmyla <savelieva0509@gmail.com>
2022-09-07 22:38:29 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsElContainer = document.querySelector('#ingredients');
+
+const elements = ingredients.map(option => {
+  const ingredientsEl = document.createElement("li");
+  ingredientsEl.classList.add("item");
+  ingredientsEl.textContent = option;
+
+  return ingredientsEl;
+})
+
+console.log(elements);
+
+ingredientsElContainer.append(...elements)
