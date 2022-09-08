c01a5363d3b430080ceef5de16ba7cc196cc46fd
Babenko Liudmyla <savelieva0509@gmail.com>
2022-09-08 21:35:30 +0300

    task-3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,29 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// const galleryContainerEl = document.querySelector(".gallery")
+
+// const galleryEl = images.map(element => {
+//   const itemEl = document.createElement("li")
+//   const imgEl = document.createElement("img")
+//   imgEl.alt = element.alt;
+//   imgEl.src = element.url;
+//   itemEl.append(imgEl);
+
+//   return itemEl;
+
+// })
+// galleryContainerEl.append(...galleryEl);
+// console.log(galleryContainerEl);
+  
+const galleryContainerEl = document.querySelector(".gallery")
+
+const galleryEl = images.map(({ alt, url }) => {
+  return `<li><img src="${url}" alt="${alt}" class = "gallery__item"> </li>`
+})
+
+const markup = galleryEl.join(" ");
+
+galleryContainerEl.insertAdjacentHTML("beforeend", markup);
+console.log(galleryContainerEl);
