60b37f06492208f00857ebcf0e43a87ab741c19a
Babenko Liudmyla <savelieva0509@gmail.com>
2022-09-09 19:28:08 +0300

    task-6

---
+++ js/task-06.js
@@ -0,0 +1,25 @@
+// Напиши скрипт, который при потере фокуса на инпуте(событие blur),
+// проверяет его содержимое на правильное количество введённых символов.
+
+const input = document.querySelector('#validation-input')
+const inputLength = input.getAttribute('data-length')
+console.log(inputLength);
+
+
+input.addEventListener('blur', onInputBlur)
+
+
+function onInputBlur(event) {
+    const isValid = event.currentTarget.value.length; 
+    console.log(isValid);
+    if (isValid >= Number(inputLength)) {
+        event.currentTarget.classList.add('valid');
+        event.currentTarget.classList.remove('invalid');
+    }
+    
+    else {
+        event.currentTarget.classList.add('invalid');
+        event.currentTarget.classList.remove('valid');
+    }
+}
+
