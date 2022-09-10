1dcf6ca8941d83486feb8ba665ee40b43650b572
Babenko Liudmyla <savelieva0509@gmail.com>
2022-09-10 20:28:23 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const formRef = document.querySelector('.login-form')
+
+
+formRef.addEventListener("submit", handleSubmit)
+
+function handleSubmit (event) {
+    event.preventDefault()  
+    const { elements: {email, password}
+    } = event.currentTarget
+    
+    if (email.value === ""|| password.value === "") {
+        alert("Все поля должны быть заполнены.")
+        return
+    }
+    
+    const userData = {
+        email: email.value,
+        password: password.value,
+        
+    }
+    console.log(userData)
+    formRef.reset()
+}
+
+
 
